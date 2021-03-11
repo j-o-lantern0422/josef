e5dad752952779895350ed33fdcb41237f529cf8
jolantern <j.o.lantern0422@gmail.com>
2021-03-11 23:19:23 +0900

    module name change to remote

--- lib/josef/cli.rb
+++ lib/josef/cli.rb
@@ -3,7 +3,7 @@ require "josef/dump"
 module Josef
   class Cli < Thor
     include Josef::GoogleWorkspace::Client
-    include Josef::Dump
+    include Josef::Remote
     class_option :dry_run, :type => :boolean, :default => false
 
     desc "dump", "dump google workspace group"
--- lib/josef/remote.rb
+++ lib/josef/remote.rb
@@ -2,7 +2,7 @@ require "josef/google_workspace/client"
 require "active_support"
 require "active_support/core_ext"
 module Josef
-  module Dump
+  module Remote
     include Josef::GoogleWorkspace::Client
     def remote_dump
       YAML.dump(remote.map{|h| h.deep_stringify_keys}, File.open('dump.yml', 'w'))
